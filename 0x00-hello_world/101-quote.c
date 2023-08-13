bbd7db318e86fcd9adf415a84e66beb91cce330b
Temi <topsynumix@gmail.com>
2023-08-12 20:44:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A C program that prints a line to the standard error
- * Retturn 0 (Success)
+ * Return 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1)
+	write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
