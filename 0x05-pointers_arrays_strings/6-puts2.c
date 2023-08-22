2bc65354c741ff07c026532639906d2a87cb7c57
Temi <topsynumix@gmail.com>
2023-08-21 21:20:27 -0700

    Weldone

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string,
+ * starting with the first character.
+ * @str: The string to be printed.
+ */
+void puts2(char *str)
+{
+	int l = 0;
+	int i = 0;
+	char *y = str;
+	int j;
+
+	/*Calculate the length of the string*/
+	for (; *y != '\0'; y++)
+	{
+		l++;
+	}
+
+	/*Calculate the index of the last character*/
+	i = l - 1;
+
+	/*Iterate through the string and print every other character*/
+	for (j = 0; j <= i; j++)
+	{
+		if (j % 2 == 0)
+		{
+			putchar(str[j]);
+		}
+	}
+
+	putchar('\n');
+}
